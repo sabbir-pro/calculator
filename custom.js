06da98f3488de6874e9fd93803ddd08765da70ee
sabbir-pro <108140587+sabbir-pro@users.noreply.github.com>
2022-06-24 21:25:35 +0600

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,40 @@
+"use strict";
+
+let outputScreen = document.getElementById("output-screen");
+
+function disply (num) {
+
+    outputScreen.value += num;
+
+}
+
+function Calculate () {
+
+    try {
+
+        outputScreen.value = eval (outputScreen.value);
+
+    }
+
+    catch (err)
+
+    {
+
+        alert("Invalid Input");
+
+    }
+
+}
+
+function Clear () {
+
+    outputScreen.value = ""
+
+}
+
+function del () {
+
+    outputScreen.value = outputScreen.value.slice(0, -1);
+
+}
+
